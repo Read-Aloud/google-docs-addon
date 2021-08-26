02575d0b0166db845fb99131d48c9bbe25925b40
Hai Phan <hai.phan@gmail.com>
2021-08-25 22:36:26 -0700

    basic working

--- index.ts
+++ index.ts
@@ -22,3 +22,13 @@ function showSidebar() {
     .setTitle('Read Aloud')
   DocumentApp.getUi().showSidebar(html)
 }
+
+function getTexts() {
+  const body = DocumentApp.getActiveDocument().getBody()
+  const children = []
+  for (let i=0; i<body.getNumChildren(); i++) children.push(body.getChild(i))
+  return children
+    .filter(child => child.getType() === DocumentApp.ElementType.PARAGRAPH)
+    .map(child => child.getText().trim())
+    .filter(text => text)
+}
