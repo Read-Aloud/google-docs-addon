50f6295b21719a149d7da15e3cc4ccdf84db9c6c
Hai Phan <hai.phan@gmail.com>
2021-08-26 16:22:47 -0700

    handle list and table

--- index.ts
+++ index.ts
@@ -25,10 +25,34 @@ function showSidebar() {
 
 function getTexts() {
   const body = DocumentApp.getActiveDocument().getBody()
-  const children = []
+  const children: GoogleAppsScript.Document.Element[] = []
   for (let i=0; i<body.getNumChildren(); i++) children.push(body.getChild(i))
   return children
-    .filter(child => child.getType() === DocumentApp.ElementType.PARAGRAPH)
-    .map(child => child.getText().trim())
+    .filter(child => {
+      switch (child.getType()) {
+        case DocumentApp.ElementType.PARAGRAPH:
+        case DocumentApp.ElementType.LIST_ITEM:
+        case DocumentApp.ElementType.TABLE:
+          return true
+        default:
+          return false
+      }
+    })
+    .flatMap(child => {
+      switch (child.getType()) {
+        case DocumentApp.ElementType.PARAGRAPH:
+          return child.asParagraph().getText().trim()
+        case DocumentApp.ElementType.LIST_ITEM:
+          const item = child.asListItem()
+          const index = children
+            .filter(c => c.getType() == DocumentApp.ElementType.LIST_ITEM && c.asListItem().getListId() == item.getListId())
+            .findIndex(c => c == child)
+          const itemNumber = index + 1
+          const itemText = item.getText().trim()
+          return itemNumber && itemText ? `${itemNumber}. ${itemText}` : itemText
+        case DocumentApp.ElementType.TABLE:
+          return child.asTable().getText().trim()
+      }
+    })
     .filter(text => text)
 }
